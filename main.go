bb83e3f9a1e9eb554120a85254c97c9974f983c8
碧海青天 <74009293+bh-qt@users.noreply.github.com>
2024-07-07 10:44:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ var (
 	outFile      = flag.String("outfile", "ip.csv", "输出文件名称")                              // 输出文件名称
 	maxThreads   = flag.Int("max", 100, "并发请求最大协程数")                                       // 最大协程数
 	speedTest    = flag.Int("speedtest", 5, "下载测速协程数量,设为0禁用测速")                            // 下载测速协程数量
-    speedTestURL = flag.String("url", "speed.cloudflare.com/__down?bytes=500000000", "测速文件地址") // 测速文件地址
+    speedTestURL = flag.String("url", "speed.bihai.cf/__down?bytes=500000000", "测速文件地址") // 测速文件地址
 	enableTLS    = flag.Bool("tls", true, "是否启用TLS")                                       // TLS是否启用
 	TCPurl       = flag.String("tcpurl", "www.speedtest.net", "TCP请求地址")                   // TCP请求地址
 )
@@ -39,8 +39,9 @@ type result struct {
 	ip          string        // IP地址
 	port        int           // 端口
 	dataCenter  string        // 数据中心
+
 	region      string        // 地区
-	cca2        string        // 顶级域名后缀	
+	cca2        string 
 	city        string        // 城市
 	latency     string        // 延迟
 	tcpDuration time.Duration // TCP请求延迟
@@ -86,8 +87,8 @@ func main() {
 
 	var locations []location
 	if _, err := os.Stat("locations.json"); os.IsNotExist(err) {
-		fmt.Println("本地 locations.json 不存在\n正在从 https://speed.cloudflare.com/locations 下载 locations.json")
-		resp, err := http.Get("https://speed.cloudflare.com/locations")
+		fmt.Println("本地 locations.json 不存在\n正在从 https://speed.bihai.cf/locations 下载 locations.json")
+		resp, err := http.Get("https://speed.bihai.cf/locations")
 		if err != nil {
 			fmt.Printf("无法从URL中获取JSON: %v\n", err)
 			return
